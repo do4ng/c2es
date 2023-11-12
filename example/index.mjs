84b74f75dcbd61d958338bff7b26c75015ca3408
r.ehdals <57896501+do4ng@users.noreply.github.com>
2023-11-12 13:25:08 +0900

    chore(example): filename

---
+++ example/index.mjs
@@ -0,0 +1,6 @@
+/*1*/ import { readFileSync as $$require_readFileSync } from 'fs';
+/*1.1*/ /*2*/ var module = { exports: {} };
+/*2.1*/ var readFileSync = $$require_readFileSync;
+
+console.log(readFileSync('./main.js', 'utf-8'));
+/*3*/ export default module; /*3.1*/
--- example/output.js
+++
@@ -1,6 +0,0 @@
-import { readFileSync as _require_readFileSync } from 'fs';
-var module = { exports: {} };
-var readFileSync = _require_readFileSync;
-
-console.log(readFileSync('./main.js', 'utf-8'));
-export default module;
